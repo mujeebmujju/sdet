2e92e0fc6ffe455dad06ebf0947d77b573a2e4fb
mujeebmujju <Mujeeburrehaman94>
2023-11-29 15:55:05 +0530

    New commit

--- src/test/java/SecondPackage/SimplegitTest.java
+++ src/test/java/SecondPackage/SimplegitTest.java
@@ -1,6 +1,6 @@
 package SecondPackage;
 
-public class Simplegit 
+public class SimplegitTest 
 {
 
 	public static void main(String[] args) 
@@ -8,7 +8,9 @@ public class Simplegit
 		
 		System.out.println("Hello World");
 		System.out.println("Hello mujeeb");
-		System.out.println("Hello salma");
+		System.out.println("Hello manoj");
+		System.out.println("hi");
+		
 
 	}
 }
